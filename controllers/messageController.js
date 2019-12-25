7aadf36e0cb8a2e95ccec086b5c96723c4103f87
ThyTran <huethytran25@gmail.com>
2019-12-26 01:52:30 +0700

    fix chat

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -24,6 +24,7 @@ exports.addMessage = async (req, res)=>{
         users: arr,
         message: []
     }
+    if (temp.users[0] == null || temp.users[1] == null) res.status(400).json({message: "userID null"});
     temp.message.push(req.body.message);
     const temp1 = await Message.findOne({users: { $all: temp.users}});
     if (!temp1){
@@ -37,19 +38,30 @@ exports.addMessage = async (req, res)=>{
     else {
         var temp2 = temp1.message;
         temp2.push(req.body.message);
+        if (temp1.message[temp1.message.length-2].author == req.body.message.author ){
         const message = await Message.update({users: { $all: temp.users}}, { $set: {message: temp2, newMessage: true, time: new Date()}, $inc: {numOfNewMessages: 1}});
         if (!message) res.status(400).json({message: "Add message failed"});
     else{
         const u = await User.update({_id:`${req.body.userID}`}, {$inc:{newMessages: 1}});
-        console.log("uuuuuu", req.body.userID)
          res.status(200).json({message: "Add message success"});
     }
+}
+        else {
+            const temp3 = await User.findByIdAndUpdate(req.body.id, {$inc: {newMessages: -temp1.numOfNewMessages}})
+            const message = await Message.update({users: { $all: temp.users}}, { $set: {message: temp2, newMessage: true, time: new Date(), numOfNewMessages: 1}});
+            if (!message) res.status(400).json({message: "Add message failed"});
+        else{
+            const u = await User.update({_id:`${req.body.userID}`}, {$inc:{newMessages: 1}});
+             res.status(200).json({message: "Add message success"});
+        }
+        }
     }
 }
 exports.get4Messages = (req, res)=>{
     Message.find({users: `${req.user._id}`}).sort({time: -1}).skip(parseInt(req.query.data)).limit(4).exec(async (err, temp)=>{
     if (!temp) res.status(400).json({message: "Load messages failed"});
     else{
+        console.log(temp, "temppp", req.user._id)
         var kq =[];
         for (let i=0;i<temp.length; i++){
             var id = req.user._id == temp[i].users[0]? temp[i].users[1] : temp[i].users[0];
@@ -60,7 +72,7 @@ exports.get4Messages = (req, res)=>{
                 name: user.name,
                 id: user._id,
                 picture: user.picture,
-                lastMessage: temp[i].message[temp[i].message.length -1].data.text,
+                lastMessage: temp[i].message[temp[i].message.length -1].data.text == undefined? temp[i].message[temp[i].message.length -1].data.emoji : temp[i].message[temp[i].message.length -1].data.text,
                 newMessage: temp[i].newMessage,
                 lastMessageAuthor: temp[i].message[temp[i].message.length -1].author == user._id? user.name : "You"
             }
--- socket.js
+++ socket.js
@@ -3,14 +3,14 @@ const jwt = require('jsonwebtoken');
 exports.socketio = function (socket){
     socket.on("Client-login", async (data)=>{
         var verify = jwt.verify(data, process.env.TOKEN_SECRET);
-        console.log("verify",verify);
-        console.log(socket.adapter.rooms, socket.id);
+        console.log("Thy khùng",verify);
+        
         const temp = await Socket.update({userID: verify.userID},{$set:{socketID: socket.id}});
         if (!temp) socket.emit("Change-socketid-failed");
         else socket.emit("Change-socketid-sucessfully");
       });
       socket.on("Client-send-message", async (data) =>{
-        console.log("sockettttt", data)
+        console.log("Trần Huệ Thyyyyyyyyyyyyyyyyyyyyyyy", data)
         const temp = await Socket.find({userID: data.userID});
         console.log(temp)
         if (!temp) console.log("socket Client send message failed");
@@ -20,7 +20,11 @@ exports.socketio = function (socket){
             picture: data.picture,
             userID: data.id
           }
-          console.log("send");
+          console.log("send", temp[0].socketID);
         socket.broadcast.in(temp[0].socketID).emit("Client-send-message", {user, message: data.message});
       }})
+      socket.on("Client-disconnect",function(){
+        socket.disconnect(true);
+        console.log(socket.adapter.rooms, socket.id);
+      })
 }
